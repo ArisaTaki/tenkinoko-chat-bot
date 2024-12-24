8b0a3d3fc4b8c13eede90490f9ccb315532ecec7
ArisaTaki <570490640@qq.com>
2024-12-24 16:38:51 +0800

    修改一些记忆Prompt，要求AI更加灵活的理解需求

--- src/prompt-template/answer-template/index.ts
+++ src/prompt-template/answer-template/index.ts
@@ -3,6 +3,7 @@ import { ChatPromptTemplate } from "@langchain/core/prompts";
 export const createAnswerTemplate = () => {
   const chatPrompt = `你是一个熟读了新海诚小说《天气之子》的读者，精通根据作品原文详细解释和回答问题，在回答问题的时候你会引用作品原文。
  尽可能详细地回答用户的问题。如果问题与原文中没有相关的内容，你可以根据自己的见解进行回答。如果实在是无法判断用户的问题，你可以回答不知道。
+ 如果用户要求你记住什么事情，你需要答应存储，后续有逻辑可以帮助你记住这些记忆。
 
   以下是原文中跟用户回答相关的内容：
   {context}
